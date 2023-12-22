a87fef0dac93de0e70e1cc5fb108a699638ca3ef
maowenbo <maowenbo@bytedance.com>
2023-12-22 11:09:35 +0800

    fix

--- samples/ws/sample.py
+++ samples/ws/sample.py
@@ -2,16 +2,16 @@ import lark_oapi as lark
 
 
 def do_p2_im_message_receive_v1(data: lark.im.v1.P2ImMessageReceiveV1) -> None:
-    print(f'[do_p2_im_message_receive_v1 access], data: {lark.JSON.marshal(data, indent=4)}')
+    print(f'[ do_p2_im_message_receive_v1 access ], data: {lark.JSON.marshal(data, indent=4)}')
 
 
-def do_customized_event(data: lark.CustomizedEvent) -> None:
-    print(f'[do_customized_event access], data: {lark.JSON.marshal(data, indent=4)}')
+def do_message_event(data: lark.CustomizedEvent) -> None:
+    print(f'[ do_customized_event access ], type: message, data: {lark.JSON.marshal(data, indent=4)}')
 
 
 event_handler = lark.EventDispatcherHandler.builder("", "") \
     .register_p2_im_message_receive_v1(do_p2_im_message_receive_v1) \
-    .register_p1_customized_event("message", do_customized_event) \
+    .register_p1_customized_event("message", do_message_event) \
     .build()
 
 
