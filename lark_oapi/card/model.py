cb0b47136925183c5f90986109bd7f4c6b63fb52
xuanmiss <xuanmine@gmail.com>
2024-07-24 00:29:55 +0800

    feat: 消息卡片回调中的新增的交互方式参数添加到Action里

--- lark_oapi/card/model.py
+++ lark_oapi/card/model.py
@@ -1,4 +1,4 @@
-from typing import Dict, Optional, Any
+from typing import Dict, Optional, Any, List
 
 from lark_oapi.core.construct import init
 from lark_oapi.core.model import RawRequest
@@ -13,6 +13,10 @@ class Action(object):
         self.option: Optional[str] = None
         self.timezone: Optional[str] = None
         self.form_value: Dict[str, Any] = {}
+        self.name: Optional[str] = None
+        self.input_value: Optional[str] = None
+        self.options: Optional[List[str]] = None
+        self.checked: Optional[bool] = None
         init(self, d, self._types)
 
 
