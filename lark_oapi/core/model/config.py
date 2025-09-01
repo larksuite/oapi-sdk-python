920e21efe335eaa447d1bf1e807a7e798eb7c394
liuzheng <liuzheng712@gmail.com>
2025-09-01 14:25:32 +0800

    feat: 添加代理支持到配置和传输模块

--- lark_oapi/core/model/config.py
+++ lark_oapi/core/model/config.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Dict, Optional
 
 from lark_oapi.core import AppType, LogLevel
 from lark_oapi.core.cache import ICache
@@ -11,7 +11,12 @@ class Config(object):
         self.app_secret: Optional[str] = None
         self.domain: str = FEISHU_DOMAIN  # 域名, 默认为 https://open.feishu.cn
         self.timeout: Optional[float] = None  # 客户端超时时间, 单位秒, 默认永不超时
-        self.app_type: AppType = AppType.SELF  # 应用类型, 默认为自建应用; 若设为 ISV 需在 request_option 中配置 tenant_key
-        self.enable_set_token: bool = False  # 是否允许手动设置 token, 默认不开启; 开启后需在 request_option 中配置 token
+        self.app_type: AppType = (
+            AppType.SELF
+        )  # 应用类型, 默认为自建应用; 若设为 ISV 需在 request_option 中配置 tenant_key
+        self.enable_set_token: bool = (
+            False  # 是否允许手动设置 token, 默认不开启; 开启后需在 request_option 中配置 token
+        )
         self.cache: Optional[ICache] = None  # 自定义缓存, 默认使用预置的本地缓存
         self.log_level: LogLevel = LogLevel.WARNING  # 日志级别, 默认为 WARNING
+        self.proxies: Optional[Dict[str, str]] = None  # 代理设置, 默认为 None
